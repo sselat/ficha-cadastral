f6f5493021c8c9ae0e48ca7fc7762d37d4f13beb
Tales B. Albuquerque <101683474+sselat@users.noreply.github.com>
2023-03-31 16:58:42 -0400

    validateCPF

--- scripts/validarcpf.js
+++ scripts/validarcpf.js
@@ -1,17 +1,26 @@
-const cpf = '01120722160'
-let cpfArray = [...cpf]
-let cpfReverse = [...cpf].reverse()
+let cpf = '011.207.221-60'
+validarCpf(cpf)
 
+function validarCpf(rawCpf) {
+  let cpf = rawCpf.slice(0,-2).replace(/[^a-zA-Z0-9]/g,'')
+  let cpfArray = [...cpf]
+  let comparationArray = [10, 9, 8, 7, 6, 5, 4, 3, 2]
 
-console.log('Array: ' + cpfArray)
-console.log('Reverse: ' + cpfReverse)
+  console.log('cpf formatado: ' + cpf)
+  console.log('Array: ' + cpfArray)
+  console.log('Reverse: ' + comparationArray)
 
-const cpfSoma = cpfArray.map((val,index) => {
-  return Number(val) + Number(cpfReverse[index])
-})
+  const arrayMultiplicado = cpfArray.map((val, index) => {
+    return Number(val) * Number(comparationArray[index])
+  })
 
-let somaTotal = cpfSoma.reduce((val, soma) => {
-  return val+soma
-},0)
+  console.log('Array soma: ' + arrayMultiplicado)
 
-console.log(valor)
+  let somaTotal = arrayMultiplicado.reduce((val, soma) => {
+    return val + soma
+  }, 0)
+
+  console.log('Soma total: ' + somaTotal)
+
+  console.log('Resto: ' + parseInt((somaTotal*10)/11))
+}
